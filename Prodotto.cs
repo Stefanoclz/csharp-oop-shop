8170bf7b68b768d0bcfaa98a13da11db62071ddb
Stefano Calzoni <stefanoclz@hotmail.it>
2022-06-29 14:36:18 +0200

    creata classe per oggetto da definire

---
+++ Prodotto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_shop
+{
+    public class Prodotto
+    {
+        public string prodName;
+        public string prodDescription;
+        public double prodPrice;
+        public int prodCode;
+
+        public Prodotto(string input1, string input2, double input3)
+        {
+            prodName = input1;
+            prodDescription = input2;
+            prodPrice = input3;
+
+            Random rand = new Random();
+            prodCode = rand.Next(1, 100);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+using csharp_oop_shop;
+
+/*Console.WriteLine("Inserisci il nome del nuovo prodotto: ");
+string name = Console.ReadLine();
+
+Console.WriteLine("Inserisci una descrizione per il nuovo prodotto: ");
+string description = Console.ReadLine();
+
+Console.WriteLine("Inserisci il prezzo del nuovo prodotto: ");
+double price = decimal.Parse(Console.ReadLine());*/
+
+
+Prodotto first = new Prodotto("rastrello", "funziona bene", 1.99);
+Console.WriteLine(first.prodName);
+
+
